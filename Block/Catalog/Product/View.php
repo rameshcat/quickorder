d57b4c1543f30d8b0e412ef2169f72a6e33ab4b3
Dima <wizald4@gmail.com>
2019-07-30 19:44:19 +0300

    first

---
+++ Block/Catalog/Product/View.php
@@ -0,0 +1,19 @@
+<?php
+ 
+namespace Roma\QuickOrder\Block\Catalog\Product;
+ 
+use Magento\Catalog\Block\Product\Context;
+use Magento\Catalog\Block\Product\AbstractProduct;
+ 
+class View extends AbstractProduct
+{
+
+    public function __construct(
+        Context $context,
+        array $data
+    ) {
+
+        parent::__construct($context, $data);
+    }
+
+}
