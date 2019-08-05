ae960b7b11ffd29f48b715d7895e995984912f36
Dima <wizald4@gmail.com>
2019-08-05 20:29:08 +0300

    status config

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -10,22 +10,26 @@ class Index extends BaseAction
 {
     protected $model;
     protected $date;
+    protected $scopeConfig;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Roma\QuickOrder\Model\QuickOrder $model,
-        \Magento\Framework\Stdlib\DateTime\DateTime $date
+        \Magento\Framework\Stdlib\DateTime\DateTime $date,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
     ) {
         $this->model = $model;
         $this->date = $date;
+        $this->scopeConfig = $scopeConfig;
         parent::__construct($context);
     }
     public function execute()
     {
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        $defaultStatus = $this->scopeConfig->getValue('quickorder/general/by_default', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
         $data = $this->getRequest()->getParams();
         $this->model->setData($data);
-        $this->model->setStatus('default');
+        $this->model->setStatus($defaultStatus);
         $this->model->setDateTime($this->date->gmtDate());
         $this->model->setUpdateDateTime($this->date->gmtDate());
 
